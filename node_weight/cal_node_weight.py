94429c7d2da17bafcd1fc003891aff5ea2d36d29
Yunfei Long <longyunf@msu.edu>
2020-04-13 12:49:08 -0400

    update

--- node_weight/cal_node_weight.py
+++ node_weight/cal_node_weight.py
@@ -12,10 +12,12 @@ if __name__ == '__main__':
     design_matrix = pd.read_csv(join(dir_data, 'design_matrix.csv'))   
     count_matrix = pd.read_csv(join(dir_data, 'count_matrix.csv'))
     
-    design_matrix = design_matrix.set_index(design_matrix.columns[0])
-    count_matrix = count_matrix.set_index(count_matrix.columns[0])
+    design_matrix.index = design_matrix['Cultivar']
     
-    
+    count_matrix = count_matrix.stack()
+    count_matrix[ pd.isna(count_matrix) ] = 0
+    count_matrix = count_matrix.unstack()
+       
     dds = py_DESeq2(count_matrix,
                design_matrix,
                design_formula = '~ sample',
